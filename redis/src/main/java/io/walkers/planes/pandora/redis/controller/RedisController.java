e6d07866bfe669f96f0f72182eb68e2bec87ab2f
fan <fanyidongdong@gmail.com>
2021-10-21 15:04:34 +0800

    feat: add distributed lock interface

--- redis/src/main/java/io/walkers/planes/pandora/redis/controller/RedisController.java
+++ redis/src/main/java/io/walkers/planes/pandora/redis/controller/RedisController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * redis操作示例 {@link RedisTemplate}
@@ -25,6 +26,8 @@ public class RedisController {
     private RedisTemplate<String, String> redisTemplate;
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private RedisService redisService;
 
     @GetMapping("/redis/get")
     public String getValueFromRedis(String id) throws JsonProcessingException {
@@ -45,5 +48,62 @@ public class RedisController {
         }
         System.out.println(userString);
         return userString;
+    }
+
+    /**
+     * http://127.0.0.1:8080/redis/lock?key=fanyidong&value=lock&version=3
+     * @param version
+     * @param key
+     * @param value
+     * @return
+     * @throws InterruptedException
+     */
+    @GetMapping("/redis/lock")
+    public Object lock(int version, String key, String value) throws InterruptedException {
+        boolean islock = false;
+        long now = System.currentTimeMillis();
+        if (version==1) {
+            // 存在分布式死锁问题
+            islock = redisService.lock1(key, value);
+        } else if (version==2) {
+            // 存在自动解锁问题 && 存在无法区分不同客户端操作的问题
+            // todo 通过 Redission 的 wtach dog 为分布式锁自动续期，定时监控此key，若依旧被该客户端持有，更新过期时间
+            islock = redisService.lock2(key, RedisService.ServerSingle + now + 10000, 10, TimeUnit.SECONDS);
+        } else if (version==3) {
+            // 存在自动解锁问题
+            islock = redisService.lock3(key, RedisService.ServerSingle + now + 10000, 10, TimeUnit.SECONDS);
+        }
+        // 加锁成功
+        if (!islock) {
+            return false;
+        }
+        // 处理业务大于过期时间
+//        TimeUnit.SECONDS.sleep(11);
+        // 解锁
+        return redisService.unlock1(key);
+    }
+
+    /**
+     * 测试可重入锁
+     * http://127.0.0.1:8080/redis/lockAgain?key=fanyidong
+     * @param key
+     * @return
+     */
+    @GetMapping("redis/lockAgain")
+    public Object lockAgain(String key) {
+        if (redisService.get(key)==null) {
+            return false;
+        }
+        return redisService.lock3(key, RedisService.ServerSingle + System.currentTimeMillis() + 10000, 10, TimeUnit.SECONDS);
+    }
+
+    /**
+     * http://127.0.0.1:8080/redis/get?key=fanyidong
+     * @param key
+     * @return
+     */
+    @GetMapping("/redis/get")
+    public Object get(String key) {
+        return redisService.get(key);
     }
 }
