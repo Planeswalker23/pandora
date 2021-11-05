21f3a78a9c95f24ac0399a680700cce956720b18
Planeswalker23 <fanyidongdong@gmail.com>
2021-11-05 08:29:28 +0800

    feat(Spring): Bean作用域

---
+++ spring/src/main/java/io/walkers/planes/pandora/ioc/scpoe/IocDemo4BeanScope.java
@@ -0,0 +1,32 @@
+package io.walkers.planes.pandora.ioc.scpoe;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Bean Scope 作用域
+ * singleton: 加载 Spring 配置文件时候就会创建单实例对象
+ * prototype: 不是在加载 spring 配置文件时候创建对象，而是在调用 getBean 方法时候创建多实例对象
+ *
+ * @author planeswalker23
+ * @see org.springframework.beans.factory.FactoryBean
+ */
+public class IocDemo4BeanScope {
+
+    @Test
+    public void beanScope() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("ioc/ioc-demo4-bean-scope.xml");
+        // singleton
+        User singletonUser = applicationContext.getBean("singletonUser", User.class);
+        User singletonUser2 = applicationContext.getBean("singletonUser", User.class);
+        System.out.println(singletonUser);
+        System.out.println(singletonUser2);
+
+        // prototype
+        User prototypeUser = applicationContext.getBean("prototypeUser", User.class);
+        User prototypeUser2 = applicationContext.getBean("prototypeUser", User.class);
+        System.out.println(prototypeUser);
+        System.out.println(prototypeUser2);
+    }
+}
---
+++ spring/src/main/java/io/walkers/planes/pandora/ioc/scpoe/User.java
@@ -0,0 +1,15 @@
+package io.walkers.planes.pandora.ioc.scpoe;
+
+/**
+ * 基于 XML 配置文件创建对象
+ *
+ * @author planeswalker23
+ */
+public class User {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
