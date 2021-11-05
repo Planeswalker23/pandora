7f2ddcb46e4fdef8fd1b7d7d9893cdda45a56611
Planeswalker23 <fanyidongdong@gmail.com>
2021-11-05 08:35:47 +0800

    feat(Spring): 自动装配注入属性

---
+++ spring/src/main/java/io/walkers/planes/pandora/ioc/autowire/VipCustomer.java
@@ -0,0 +1,22 @@
+package io.walkers.planes.pandora.ioc.autowire;
+
+/**
+ * VIP 会员
+ *
+ * @author planeswalker23
+ */
+public class VipCustomer {
+
+    private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "VipCustomer{" +
+                "user=" + user +
+                '}';
+    }
+}
