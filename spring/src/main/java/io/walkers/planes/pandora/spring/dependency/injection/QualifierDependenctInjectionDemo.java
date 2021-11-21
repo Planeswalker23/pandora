b79630d7f8d8548c245ea1484784b55137d9ed20
Planeswalker23 <fanyidongdong@gmail.com>
2021-11-21 12:22:35 +0800

    feat(Spring): @Qualifier 依赖限定注入

---
+++ spring/src/main/java/io/walkers/planes/pandora/spring/dependency/injection/QualifierDependenctInjectionDemo.java
@@ -0,0 +1,76 @@
+package io.walkers.planes.pandora.spring.dependency.injection;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import java.util.List;
+
+/**
+ * 基于 {@link Qualifier} 注解进行限定注入（分组）
+ *
+ * @author planeswalker23
+ * @date 2021/11/21
+ */
+@Configuration
+public class QualifierDependenctInjectionDemo {
+
+    @Autowired
+    private User user;
+    @Autowired
+    private List<User> allUsers;
+    @Autowired
+    @Qualifier
+    private List<User> qualifierUsers;
+    @Autowired
+    @Qualifier("custom")
+    private List<User> qualifierCustomUsers;
+
+    @Test
+    public void qualifier() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(QualifierDependenctInjectionDemo.class);
+        QualifierDependenctInjectionDemo bean = applicationContext.getBean(QualifierDependenctInjectionDemo.class);
+        System.out.println("user = " + bean.user);
+        System.out.println("allUsers = " + bean.allUsers);
+        System.out.println("qualifierUsers = " + bean.qualifierUsers);
+        System.out.println("qualifierCustomUsers = " + bean.qualifierCustomUsers);
+
+    }
+
+    @Bean
+    @Primary
+    public User user1() {
+        return new User("user1");
+    }
+
+    @Bean
+    @Qualifier("custom")
+    public User user2() {
+        return new User("custom");
+    }
+
+    @Bean
+    @Qualifier
+    public User user3() {
+        return new User("user3");
+    }
+
+    public static class User {
+        private String name;
+
+        public User(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
+    }
+}
